c396835e9b4a1c42f777d794eb0fefb4607e61f6
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-10-31 20:07:29 +0100

    U3: shapes and colors in plot

--- U3/analysis.R
+++ U3/analysis.R
@@ -6,6 +6,7 @@ library(ggplot2)
 cars = read.csv("04cars.csv")
 cars$Length = as.numeric(cars$Length)
 cars$VehicleName = as.character(cars$VehicleName)
+cars$CMPG = as.numeric(cars$CMPG)
 
 cars = cars[cars$Pickup == 0,] # Pickups are missing length attribute
 Types = rep("Not Minivan",nrow(cars))
@@ -15,4 +16,12 @@ Types[cars$Wagon == 1] = "Not Minivan"
 Types[cars$Minivan == 1] = "Minivan"
 cars$Type = Types
 
-qplot(cars$InvoicePrice, cars$Length, colour=cars$Type)
+MinivanAvg = aggregate(cars$CMPG, by=list(cars$Type), FUN=mean)$x[1]
+Ecos = rep("Not Minivan", nrow(cars))
+Ecos[(cars$Minivan == 1) & (cars$CMPG < MinivanAvg)] = "Not Eco"
+Ecos[cars$Minivan == 1 & cars$CMPG >= MinivanAvg] = "Eco"
+cars$Eco = Ecos
+
+Type = cars$Type
+Eco = cars$Eco
+qplot(cars$InvoicePrice, cars$Length, colour=Type, shape=Eco, xlab="Invoice Price", ylab="Length")

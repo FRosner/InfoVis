1f616346cf0a8dfda215e7ce2216e654b06d7f1e
Frank Rosner <frank.rosner@student.uni-halle.de>
2013-11-05 11:27:54 +0100

    U3: fix conversion bug

--- U3/analysis.R
+++ U3/analysis.R
@@ -3,10 +3,10 @@ rm(list = ls())
 library(e1071)
 library(ggplot2)
 
-cars = read.csv("04cars.csv")
+cars = read.csv("04cars.csv" )
 cars$Length = as.numeric(as.character(cars$Length))
 cars$VehicleName = as.character(cars$VehicleName)
-cars$CMPG = as.numeric(cars$CMPG)
+cars$CMPG = as.numeric(as.character(cars$CMPG))
 
 Types = rep("Not Minivan",nrow(cars))
 Types[cars$SUV == 1] = "Not Minivan"
@@ -22,7 +22,7 @@ Ecos[cars$Minivan == 1 & cars$CMPG >= MinivanAvg] = "Eco"
 cars$Eco = Ecos
 cars$Type[cars$Eco == "Eco"] = "Eco Minivan"
 
-svg("EcoMinivans.svg", width=9, height=5)
+svg("EcoMinivans_plot.svg", width=9, height=5)
 p = ggplot(cars, aes(x = InvoicePrice, y=Length, colour=Type)) + geom_point() + theme_bw()
 p = p + scale_x_continuous(breaks=seq(0,200000,10000)) + scale_y_continuous(breaks=seq(0,250,10))
 p + scale_colour_manual(name="Minivan Type",labels=c("Eco", "Normal", "No Minivan"), values=c("limegreen", "lightpink2", "grey80"))
